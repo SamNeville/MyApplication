4c4c7b4a5900b608c00fcff84930d992683aed18
SamNeville <samuel.neville@snc.edu>
2019-04-23 23:22:07 -0500

    First Commit

--- app/src/main/java/com/example/myapplication/Activities/ImportDataActivity.java
+++ app/src/main/java/com/example/myapplication/Activities/ImportDataActivity.java
@@ -1,10 +1,19 @@
 package com.example.myapplication.Activities;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.io.BufferedReader;
 import java.io.Console;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.InputStreamReader;
 import java.net.URLConnection;
 import java.util.logging.ConsoleHandler;
@@ -19,33 +28,96 @@ import java.io.BufferedInputStream;
 import com.example.myapplication.R;
 
 public class ImportDataActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        final Intent intent = new Intent(ImportDataActivity.this, QuickGameActivity.class);
+        final Bundle b = new Bundle();
+        final String[] words = new String[10];
+
         setContentView(R.layout.activity_import_data);
+        Button scienceButton = findViewById(R.id.button5);
+        Button carsButton = findViewById(R.id.button6);
+        Button animalsButton = findViewById(R.id.button7);
 
+            scienceButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v)
+            {
+                try
+                {
+                    URL url = new URL("http://compsci02.snc.edu/~nevisw/csci322/ClientServerProj/science.txt");
 
-try {
+                    String line;
+                    String text = new String() ;
+                    BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+                    while ((line = in.readLine()) != null) {
+                        if(line != null)
+                        text = text + line;
+                    }
+                    in.close();
+                    String[] gameWords = new String[10];
+                    String delims = "[,]+";
+                    gameWords = text.split(delims);
+                    b.putStringArray("array", words);
+                    intent.putExtras(b);
+                    startActivity(intent);
+                } catch (MalformedURLException e) {
+                } catch (IOException e) {
+                }
+            }
+        });
+        carsButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                try
+                {
+                    URL url = new URL("http://compsci02.snc.edu/~nevisw/csci322/ClientServerProj/cars.txt");
+                    String line;
+                    String text = new String() ;
+                    BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+                    while ((line = in.readLine()) != null) {
+                        if(line != null)
+                            text = text + line;
+                    }
+                    in.close();
 
-    URL url = new URL("http://compsci02.snc.edu/~nevisw/csci322/ClientServerProj/animals.php");
-    URLConnection connection = url.openConnection();
-    connection.connect();
-    BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-    String inputLine;
-
-    while((inputLine = in.readLine()) != null)
-    {
-        System.out.println(inputLine);
-    }
-
-    in.close();
-    }catch(IOException ex)
-        {
-        int test = 0;
-
+                    String[] gameWords = new String[10];
+                    String delims = "[,]+";
+                    gameWords = text.split(delims);
+                    b.putStringArray("array", words);
+                    intent.putExtras(b);
+                    startActivity(intent);
+                } catch (MalformedURLException e) {
+                } catch (IOException e) {
+                }
+            }
+        });
+        animalsButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+            try
+            {
+                URL url = new URL("http://compsci02.snc.edu/~nevisw/csci322/ClientServerProj/animals.txt");
+                String line;
+                String text = new String() ;
+                BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+                while ((line = in.readLine()) != null) {
+                    if(line != null)
+                        text = text + line;
+                }
+                in.close();
+                String[] gameWords = new String[10];
+                String delims = "[,]+";
+                gameWords = text.split(delims);
+                b.putStringArray("array", words);
+                intent.putExtras(b);
+                startActivity(intent);
+            } catch (MalformedURLException e) {
+            } catch (IOException e) {
+            }
         }
+        });
     }
-
-
 }
