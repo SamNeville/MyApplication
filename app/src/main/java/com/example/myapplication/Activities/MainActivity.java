36a6bb62ec95a06f6fe19d86cbf0d24f16ac6769
SamNeville <samuel.neville@snc.edu>
2019-04-24 01:38:22 -0500

    First Commit

--- app/src/main/java/com/example/myapplication/Activities/MainActivity.java
+++ app/src/main/java/com/example/myapplication/Activities/MainActivity.java
@@ -2,24 +2,44 @@
 package com.example.myapplication.Activities;
 
         import android.content.Intent;
+        import android.os.StrictMode;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
         import android.view.View;
+        import android.webkit.WebChromeClient;
+        import android.webkit.WebSettings;
+        import android.webkit.WebView;
+        import android.webkit.WebViewClient;
         import android.widget.Button;
+        import android.widget.VideoView;
 
         import com.example.myapplication.R;
+
+        import java.io.BufferedReader;
+        import java.io.IOException;
+        import java.io.InputStream;
+        import java.io.InputStreamReader;
+        import java.net.HttpURLConnection;
+        import java.net.URL;
+
+        import static android.net.Uri.decode;
 
 public class MainActivity extends AppCompatActivity {
     private Button quickGameButton;
     private Button createGameButton;
     private Button importButton;
     private Button resumeGameButton;
+    VideoView video;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initialize();
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
     }
 
     public void initialize(){
@@ -83,6 +103,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ResumeGameActivity.class);
         startActivity(intent);
     }
+
 
 
 }
